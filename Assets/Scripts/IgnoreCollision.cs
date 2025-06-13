f04a6c74e92493dbdf2dac5878a5d123583298ab
maramiia <147627558+maramiia@users.noreply.github.com>
2025-06-13 17:46:38 +0500

    4workshop

---
+++ Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    public Collider colliderCube1;
+    public Collider colliderCube2;
+
+    void Start()
+    {
+        Physics.IgnoreCollision(colliderCube1,colliderCube2);
+    }
+}
